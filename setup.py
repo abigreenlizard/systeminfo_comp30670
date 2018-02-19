469d951b549c0f336bf5ff213bad8bfccbd52722
Conor Hopkins <conor.hopkins@ucdconnect.ie>
2018-02-19 13:25:14 +0000

    First Commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name="systeminfo",
+    version="0.01",
+    description="System information for comp30670",
+    url="",
+    author="Conor Hopkins",
+    author_email="conor.hopkins@ucdconnect.ie",
+    licence="GPL3",
+    packages=['systeminfo'],
+    entry_points={
+    'console_scripts':['comp30670_systeminfo=systeminfo.main:main']
+    }
+)
+
