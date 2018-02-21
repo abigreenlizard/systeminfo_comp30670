333f8f2291ea2703969a25060ad7fa22303995c7
Conor Hopkins <conor.hopkins@ucdconnect.ie>
2018-02-21 13:28:05 +0000

    Moved platform_specs() to __init__.py

---
+++ systeminfo/__init__.py
@@ -0,0 +1,8 @@
+import platform
+
+def platform_specs():
+    print(platform.platform())
+    return
+
+if __name__=="__main__":
+    platform_specs()
--- systeminfo/main.py
+++
@@ -1,8 +0,0 @@
-import platform
-
-def platform_specs():
-    print(platform.platform())
-    return
-
-if __name__=="__main__":
-    platform_specs()
